58fa20e44d40eff5853f223a14e626970986221f
DhakAres <davidanthonyhidalgo@gmail.com>
2024-04-30 23:01:10 -0500

    Temas y creacion del homeScreen

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert( selectedColor >= 0, 'Tiene que ser mayor igual a 0' ),  
+      assert( selectedColor < colorList.length, 
+        'Tiene que ser menor igual a ${ colorList.length - 1 }');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ],
+    //configuracion por defecto de todos los appbars
+    appBarTheme: const AppBarTheme(
+      centerTitle: true
+    )
+  );
+
+}
